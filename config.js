e2ec2adfbc0de01264659c40724cc97611493e9b
OveeKawre <74299130+OveeKawre@users.noreply.github.com>
2023-06-03 11:59:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD717tVIvStmWiAtx2jW7t7rnoxmPhkryI",
+    authDomain: "cook-n-bake.firebaseapp.com",
+    projectId: "cook-n-bake",
+    storageBucket: "cook-n-bake.appspot.com",
+    messagingSenderId: "790543790087",
+    appId: "1:790543790087:web:eda6cdc5867c42a3f13aa2"
+  };
